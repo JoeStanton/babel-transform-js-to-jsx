e611cda4f031749d9b88dc8f6dc27e300cfe6808
Joe Stanton <joe.stanton@red-badger.com>
2015-11-06 10:53:30 +0000

    esformatter

--- transform.js
+++ transform.js
@@ -1,5 +1,8 @@
 require("babel/register");
 
+var esformatter = require('esformatter');
+esformatter.register(require('esformatter-jsx'));
+
 var buf = "";
 process.stdin.setEncoding("utf8");
 process.stdin.on("data", function(chunk) { buf += chunk; });
