1420348706fedce2efedc2c4b52e79422254a182
Joe Stanton <joe.stanton@red-badger.com>
2015-12-22 14:30:58 +0000

    Fixed new incompatibilities

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ export default function ({types: t}) {
     });
   }
 
+  const processChildren = (children) => {
+    return children.map(c => {
+      if (t.isJSXElement(c) || t.isStringLiteral(c) || t.isJSXExpressionContainer(c)) {
+        return c;
+      } else {
+        return t.JSXExpressionContainer(c);
+      }
+    });
+  }
+
   return {
     visitor: {
       CallExpression: {
@@ -20,7 +30,7 @@ export default function ({types: t}) {
           if (Object.keys(DOM).indexOf(path.node.callee.name) === -1) return;
 
           var props = getAttributes(path.node.arguments[0]);
-          var children = path.node.arguments.slice(1);
+          var children = processChildren(path.node.arguments.slice(1));
 
           var name = t.JSXIdentifier(path.node.callee.name);
 
@@ -30,18 +40,6 @@ export default function ({types: t}) {
 
           var el = t.JSXElement(open, close, children);
           path.replaceWith(path.parent.type === 'ReturnStatement' ? t.ParenthesizedExpression(el) : t.ExpressionStatement(el));
-        }
-      },
-      JSXElement: {
-        exit: function(path) {
-          path.node.children = path.node.children.map(c => {
-            if (t.isJSXElement(c) || t.isStringLiteral(c) || t.isJSXExpressionContainer(c)) {
-              return t.isStringLiteral(c) ? t.JSXText(c.value) : c;
-            } else {
-              return t.JSXExpressionContainer(c);
-            }
-          });
-          path.replaceWith(path.node);
         }
       },
       ConditionalExpression: function(path) {
