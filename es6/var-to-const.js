f15f942ae841e81ec77897cdb80626323d71a72e
Dominik Piatek <do.piatek@gmail.com>
2017-11-22 10:44:14 +0000

    Convert var to const

---
+++ es6/var-to-const.js
@@ -0,0 +1,15 @@
+export default function ({types: t}) {
+  return {
+    visitor: {
+      VariableDeclaration(path) {
+        const declarations = path.node.declarations;
+
+        if (path.node.declarations.filter(dec => !!dec.init).length !== declarations.length) {
+          return;
+        }
+
+        path.node.kind = "const";
+      }
+    }
+  };
+}
---
+++ spec/var-to-const-spec.js
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import dedent from "dedent";
+
+const transform = (str) => {
+  return require("babel-core").transform(str, {
+    plugins: ["../es6/var-to-const"]
+  }).code
+}
+
+describe('var -> const', () => {
+  it('converts vars to const', () => {
+    const code = 'var foo = "bar";';
+    expect(transform(code)).to.equal('const foo = "bar";');
+  });
+
+  it('converts assignments sequences', () => {
+    const code = 'var foo = "bar", baz ="quux"';
+    const transformed =
+      `const foo = "bar",
+      baz = "quux";`;
+    expect(transform(code)).to.equal(transformed);
+  });
+
+  it('does not convert var declarations', () => {
+    const code = 'var foo, bar;';
+    expect(transform(code)).to.equal('var foo, bar;');
+  });
+});
+
